a77b7ea13c4fef88292adb1d04d9f626941b4b7c
WianVos <wvos@xebia.com>
2014-06-20 08:33:26 +0200

    added modulare profile

--- manifests/profile/extension.pp
+++ manifests/profile/extension.pp
@@ -1,11 +1,12 @@
 define wildfly::profile::extension(
   $target,
   $extension = $name,
-  $order = "5"
+  $order = "25"
 ){
 
   concat::fragment{"extension:${extension}":
-    content => template("wildfly/standalone/extension.xml.erb")
+    content => template("wildfly/standalone/extension.xml.erb"),
+    order   => $order
   }
 
 }
