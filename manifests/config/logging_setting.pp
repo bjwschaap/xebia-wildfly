74139ace085f8aed8aacfefee5672f6889191acd
WianVos <wvos@xebia.com>
2014-07-03 14:51:24 +0200

    fixed profile error

---
+++ manifests/config/logging_setting.pp
@@ -0,0 +1,13 @@
+class wildfly::config::loggin_setting(
+  $value,
+  $logging_file
+
+){
+  ini_setting { $name:
+    path    => $logging_file,
+    ensure  => present,
+    section => '',
+    setting => $name,
+    value   => $value
+  }
+}
